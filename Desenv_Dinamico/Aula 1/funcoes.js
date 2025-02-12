21d639cc94dade1b0d0e5c63e865d15711bb4a97
EvelynColaco <evelyncolaco0@gmail.com>
2025-02-12 15:07:09 -0400

    Add files via upload

---
+++ Desenv_Dinamico/Aula 1/funcoes.js
@@ -0,0 +1,13 @@
+// CRIANDO UMA FUNÇÃO
+function saudacao(){
+    console.log("Olá, Mundo");    
+}
+
+// PASSANDO PARÂMETROS PARA FUNÇÕES
+function novaSaudacao(nome) {
+    console.log("Seja Bem Vindo(a)", nome);
+}
+
+saudacao();
+novaSaudacao("Evelyn");
+novaSaudacao("Jhonson");
