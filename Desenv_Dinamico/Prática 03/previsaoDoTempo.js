b8e2f5f300aafae69961b002549f937ed85b3920
EvelynColaco <evelyncolaco0@gmail.com>
2025-02-13 21:08:32 -0400

    Instruções Condicionais If-Else - Previsão do Tempo

---
+++ Desenv_Dinamico/Prática 03/previsaoDoTempo.js
@@ -0,0 +1,11 @@
+// Variável que representa a previsão do tempo
+// Pode ser "rainy" (chuvoso), "sunny" (ensolarado) ou "cloudy" (nublado)
+
+let weatherForecast = "rainy";
+
+if (weatherForecast === "rainy") {
+    console.log("Devemos levar o guarda-chuva");
+} else {
+    console.log("Não devemos levar o guarda-chuva");
+}
+   
